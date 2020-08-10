dcae5844473fd8a7a79cc4b0bf4947854ab7c501
Serina Grill <gril0044@umn.edu>
2020-08-09 21:29:23 -0400

    module 2 complete

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,12 +1,28 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+    left_index = 0
+    right_index = 0
 
-    # Your code here
+    merged_arr = []
+    
+    while left_index < len(arrA) and right_index < len(arrB):
+        if arrA[left_index] > arrB[right_index]:
+            merged_arr.append(arrB[right_index])
+            right_index = right_index + 1
 
+        elif arrA[left_index] < arrB[right_index]:
+            merged_arr.append(arrA[left_index])
+            left_index = left_index + 1
+    while right_index < len(arrB):
+        merged_arr.append(arrB[right_index])
+        right_index = right_index + 1
+    while left_index < len(arrA):
+        merged_arr.append(arrA[left_index])
+        left_index = left_index + 1
+        
 
     return merged_arr
+
 arr1 = [9,8,7]
 arr2 = [6,5,4]
 print(merge(arr1,arr2))
@@ -14,9 +30,13 @@ print(merge(arr1,arr2))
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
     # Your code here
+    if len(arr) <= 1:
+        return arr
 
-
-    return arr
+    midpoint = len(arr) // 2
+    left_side = arr[:midpoint]
+    right_side = arr[midpoint:]
+    return merge(merge_sort(left_side), merge_sort(right_side))
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
